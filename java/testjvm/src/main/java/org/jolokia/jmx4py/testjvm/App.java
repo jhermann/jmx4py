4f8d50c9210647922a2bf56bfc8aedf5e2d7824d
Juergen Hermann <jh@web.de>
2011-11-03 13:07:55 +0100

    Added integration test Java app created from maven-archetype-quickstart:1.1

---
+++ java/testjvm/src/main/java/org/jolokia/jmx4py/testjvm/App.java
@@ -0,0 +1,11 @@
+package org.jolokia.jmx4py.testjvm;
+
+/**
+ *  Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
