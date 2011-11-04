1227c2d8605e68ddb4e57573f4e5c94dcb539ddb
Juergen Hermann <jh@web.de>
2011-11-04 16:33:53 +0100

    tooling fixed as far as possible

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,12 @@
 #! /usr/bin/env python
 """ Bootstrap script for setting up a development environment.
 
+    - Add "full" as an argument to install optional tools.
+    - Add "local" as an argument to force the creation of a virtual
+      environment in the working directory.
+    - Add "clean" as an argument to start fresh (remove "bin", "lib",
+      and "include" before boot-strapping).
+
     This either uses an already activated Python virtualenv (e.g. a
     pydev/Eclipse one), or creates a virtualenv in the working directory,
     and then installs the basic tooling to get you started. The script
@@ -11,6 +17,7 @@ import os
 import sys
 import glob
 import urllib2
+import shutil
 import subprocess
 
 VENV_ARGS = ["--no-site-packages"]
@@ -22,14 +29,17 @@ INSTALL_ARGS = [
 TOOLS = [ # what you need pretty much always
     "distribute>=0.6",
     "yolk",
-    "Paver >= 1.0",
-    "nose",
-    #"nosexcover",
-    #"NoseXUnit",
-    "coverage",
+    "Paver>=1.0",
+    # Version hell is as follows:
+    # - nosexcover only works with coverage==3.4, but that isn't downloadable any more, thus a patch is needed
+    # - NoseXUnit 0.3.3 has a fixed dependency to coverage==2.85, which sucks, especially since it (sometimes) works with 3.4!
+    "coverage>=3.4",
+    "nose>=0.11.1",
+    ##"?NoseXUnit", is broken
+    "nosexcover",
 ]
 EXTRA_TOOLS = [ # only needed in integration builds and for releasing
-    "sphinx",
+    "Sphinx",
     "pylint",
     "bpython",
 ]
@@ -66,16 +76,37 @@ def _get_real_python():
     return is_venv, python_exe
 
 
+def _install_dependency(dependency, venv_dir):
+    "Install helper, if dependency starts with '?' it's optional (i.e. installation errors are ignored)"
+    print
+    print "~~~ " + (dependency + ' ').ljust(74, '~')
+
+    installer = os.path.join(venv_dir, "Scripts", "easy_install.exe")
+    if not os.path.exists(installer):
+        installer = os.path.join(venv_dir, "bin", "easy_install")
+
+    call = subprocess.check_call
+    if dependency.startswith('?'):
+        dependency = dependency[1:]
+        call = subprocess.call
+    
+    call([installer] + INSTALL_ARGS + [dependency])
+
+
 def run():
     "Bootstrapper"
     if len(sys.argv) > 1 and sys.argv[1] in ("-?", "-h", "--help"):
         sys.stderr.write(__doc__.strip() + '\n')
         sys.exit(1)
 
+    if "clean" in sys.argv:
+        for dirname in ("bin", "build", "include", "lib"):
+            os.path.isdir(dirname) and shutil.rmtree(dirname)
+
     venv_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(venv_dir)
     is_venv, real_python = _get_real_python()
-    if is_venv:
+    if is_venv and "local" not in sys.argv:
         print "Using existing Python virtualenv in '%s'" % (sys.prefix,)
         if os.name == 'posix' and not os.path.isdir("bin"):
             # Provide easy access to tools
@@ -88,17 +119,21 @@ def run():
         subprocess.check_call([real_python, "venv.py"] + VENV_ARGS + [venv_dir])
         os.remove("venv.py")
 
-    installer = os.path.join(venv_dir, "Scripts", "easy_install.exe")
-    if not os.path.exists(installer):
-        installer = os.path.join(venv_dir, "bin", "easy_install")
-
     for tool in TOOLS:
-        subprocess.check_call([installer] + INSTALL_ARGS + [tool])
+        _install_dependency(tool, venv_dir)
 
-    if len(sys.argv) > 1 and sys.argv[1] == "full":
+    if "full" in sys.argv:
         for tool in EXTRA_TOOLS:
-            subprocess.check_call([installer] + INSTALL_ARGS + [tool])
-    
+            _install_dependency(tool, venv_dir)
+
+    # Fix broken nose plugins...
+    # If you want full unittest support on Windows, use cygwin/bash, or apply the patch manually
+    subprocess.call(
+        """sed -ie 's/if self.coverErase/if getattr(self, "coverErase", False)/'"""
+        """ $(find lib/ -path '*/nose/plugins/cover.py')""", shell=True
+    )
+    #sed -ie 's/coverage == 2.85/coverage >= 2.85/' \
+    #$(find lib/ -path '*/site-packages/NoseXUnit*/EGG-INFO/requires.txt')
 
 if __name__ == "__main__":
     run()
